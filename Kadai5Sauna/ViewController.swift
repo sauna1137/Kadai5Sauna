5612c5ac6276750c253d5ae98ed6902d318d0111
koichi <soneko2102@gmail.com>
2021-09-04 18:17:24 +0900

    フィードバックを受け修正

--- Kadai5Sauna/ViewController.swift
+++ Kadai5Sauna/ViewController.swift
@@ -23,49 +23,32 @@ class ViewController: UIViewController {
     }
 
     private func calculateResult() {
-        guard let num1 = Double(textFields[0].text!) else {
-            showAlert(irregulerStrings: "notEnteredNum1")
+        guard let num1 = Double(textFields[0].text! ) else {
+            presentAlert(message: "割られる数を入力して下さい")
             return
         }
 
         guard let num2 = Double(textFields[1].text!) else {
-            showAlert(irregulerStrings: "notEnteredNum2")
+            presentAlert(message: "割る数を入力して下さい")
             return
         }
 
-        if num2 == 0 {
-            showAlert(irregulerStrings: "num2==0")
+        guard num2 != 0 else {
+            presentAlert(message: "割る数に0を入力しないでください")
             textFields[1].text = ""
             return
         }
 
-        let result = num1/num2
+        let result = num1 / num2
         resultLabel.text = String(result)
         view.endEditing(true)
     }
 
-    private func showAlert(irregulerStrings: String) {
-        switch irregulerStrings {
-        case "num2==0":
-            let alertController = UIAlertController(title: "課題5", message: "割る数に0を入力しないでください", preferredStyle: .alert)
-            let alertAction = UIAlertAction(title: "OK", style: .default)
-            alertController.addAction(alertAction)
-            present(alertController, animated: true, completion: nil)
-
-        case "notEnteredNum1":
-            let alertController = UIAlertController(title: "課題5", message: "割られる数を入力して下さい", preferredStyle: .alert)
-            let alertAction = UIAlertAction(title: "OK", style: .default)
-            alertController.addAction(alertAction)
-            present(alertController, animated: true, completion: nil)
-
-        case "notEnteredNum2":
-            let alertController = UIAlertController(title: "課題5", message: "割る数を入力して下さい", preferredStyle: .alert)
-            let alertAction = UIAlertAction(title: "OK", style: .default)
-            alertController.addAction(alertAction)
-            present(alertController, animated: true, completion: nil)
-
-        default:break
-        }
+    private func presentAlert(message: String) {
+        let alertController = UIAlertController(title: "課題5", message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
